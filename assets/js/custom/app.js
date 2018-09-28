35b9ad8903cb4f91f7d1b70b95b681641e378c28
Bamise Muyiwa <muyiwa@jibixtech.com>
2018-09-28 19:13:54 +0200

    initial

---
+++ assets/js/custom/app.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $(window).stellar();
+    var gal = $('#jsGallery').jsGallery({
+        zIndex: 1000
+    });
+    
+    $('#showGal').click(() => {
+        gal.jsGallery('show', 'Album 1', 1);
+    })
+    $("body a").bind("click", function () {
+        $("html, body").stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+})
